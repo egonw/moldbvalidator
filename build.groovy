b4e34f42ea980787adc8f45a5ee5a0a88fac0376
Egon Willighagen <egon.willighagen@gmail.com>
2011-03-23 07:50:42 +0100

    A build environment

---
+++ build.groovy
@@ -0,0 +1,39 @@
+def ant = new AntBuilder()
+
+bindir = "bin"
+srcdir = "src"
+libdir = "jar"
+pkgname = "moldbvalid"
+
+ant.taskdef(
+  name:"groovyc",
+  classname:"org.codehaus.groovy.ant.Groovyc"
+)
+
+ant.mkdir(dir:"bin")
+ant.javac(
+  srcdir:"$srcdir", destdir:"$bindir",
+  includes:"**/*.java",
+  fork:"true") {
+  classpath {
+    fileset dir:"$libdir", {
+      include name:"*.jar"
+    }
+  }
+}
+ant.groovyc(
+  srcdir:"$srcdir", destdir:"$bindir",
+  includes:"**/*.groovy",
+  fork:"true") {
+  classpath {
+    fileset dir:"$libdir", {
+      include name:"*.jar"
+    }
+  }
+}
+
+ant.jar(
+  destfile:pkgname + ".jar",
+  basedir:"$bindir",
+  includes:"**/*.class"
+)
